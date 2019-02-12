751ece9245b7655dbceeea69d04b285e86080442
厉害的花花 <117415792@qq.com>
2019-02-12 18:01:23 +0800

    add server file

--- ServerCreateCFG.py
+++ ServerCreateCFG.py
@@ -1,7 +1,6 @@
 #coding:utf-8
 import os;
 import os.path;
-import requests;
 import hashlib;
 import ConfigParser;
 #----------------------------↓↓↓↓↓↓↓↓↓↓↓↓↓ 通用方法 ↓↓↓↓↓↓↓↓↓↓↓↓↓----------------------------
@@ -30,25 +29,14 @@ def download_file_save_to_path(urlString, savePath):
         save_data.write(the_request.content)
 #----------------------------↑↑↑↑↑↑↑↑↑↑↑↑↑ 通用方法 ↑↑↑↑↑↑↑↑↑↑↑↑↑----------------------------
 # 配置文件
-_config_file_path = "./update.cfg"
+_config_file_path = "./Server.cfg"
 cfg = ConfigParser.ConfigParser()
 cfg.read(_config_file_path)
-
 # 读取配置
-_minecraft_path = cfg.get('File Path', 'minecraft');
 _minecraft_mods_path = cfg.get('File Path', 'minecraft_mods');
 
-# 文件是否存在
-if os.access(_minecraft_path, os.F_OK) and os.access(_minecraft_mods_path, os.F_OK):
-    print ".minecraft and mods exits";
-else:
-    print "文件错误";
-    exit(0);
-
 # #遍历mods文件
-# for parent,dirnames,filenames in os.walk(_minecraft_mods_path):    
-#     for filename in filenames:
-#         # print filename + "===" + get_file_md5(os.path.join(parent,filename));
-#         print filename + "===" + get_string_md5(filename);
-
-download_file_save_to_path("http://127.0.0.1/forpython/[测试用jar包].jar","./[测试].jar");
+for parent,dirnames,filenames in os.walk(_minecraft_mods_path):    
+    for filename in filenames:
+        # print filename + "===" + get_file_md5(os.path.join(parent,filename));
+        print filename + "===" + get_string_md5(filename);
